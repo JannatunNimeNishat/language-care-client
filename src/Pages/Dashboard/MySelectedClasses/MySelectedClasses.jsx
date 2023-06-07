0ab934d473f31fe46180cddb75e48bd4369de33a
JannatunNimeNishat <nishatnime100@gmail.com>
2023-06-08 01:53:00 +0600

    delete from selected classes added

--- src/Pages/Dashboard/MySelectedClasses/MySelectedClasses.jsx
+++ src/Pages/Dashboard/MySelectedClasses/MySelectedClasses.jsx
@@ -1,10 +1,42 @@
 import { FaTrash } from "react-icons/fa";
 import useSelectedClass from "../../../hooks/useSelectedClass";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
+import Swal from "sweetalert2";
 
 
 const MySelectedClasses = () => {
     const [selectedClass, refetch] = useSelectedClass()
     console.log(selectedClass);
+    const [axiosSecure] = useAxiosSecure()
+
+    const handleDeleteSelectedClass = (_id) => {
+        
+        Swal.fire({
+            title: 'Are you sure?',
+            text: "You won't be able to revert this!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes, delete it!'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                axiosSecure.delete(`/selected-class-delete/${_id}`)
+                    .then(res => {
+                        console.log(res.data);
+                        refetch()
+                        Swal.fire(
+                  'Deleted!',
+                  'Your file has been deleted.',
+                  'success'
+                )
+                    })
+                
+            }
+        })
+
+
+    }
     return (
         <div className="min-h-screen w-full px-10 pt-10">
             <div className="flex items-center justify-evenly">
@@ -20,7 +52,7 @@ const MySelectedClasses = () => {
                     <thead>
                         <tr>
                             <th>
-                               #
+                                #
                             </th>
                             <th>img</th>
                             <th>Name</th>
@@ -32,9 +64,9 @@ const MySelectedClasses = () => {
                     <tbody>
                         {/* row 1 */}
                         {
-                            selectedClass?.map((singleSelectedClass,index) => <tr key={singleSelectedClass._id}>
+                            selectedClass?.map((singleSelectedClass, index) => <tr key={singleSelectedClass._id}>
                                 <th>
-                                    {index+1}
+                                    {index + 1}
                                 </th>
                                 <td>
                                     <div className="flex items-center space-x-3">
@@ -43,22 +75,22 @@ const MySelectedClasses = () => {
                                                 <img src={singleSelectedClass.img} />
                                             </div>
                                         </div>
-                                        
+
                                     </div>
                                 </td>
                                 <td>
                                     {singleSelectedClass.class_name}
-                                
+
                                 </td>
                                 <td>
                                     {singleSelectedClass.instructor_name}
-                                
+
                                 </td>
-                            
+
                                 <td>${singleSelectedClass.price}</td>
                                 <th>
-                                    <button className="btn btn-ghost btn-xs">
-                                        <FaTrash className="h-5 w-5 text-red-500"/>
+                                    <button onClick={() => handleDeleteSelectedClass(singleSelectedClass._id)} className="btn btn-ghost btn-xs">
+                                        <FaTrash className="h-5 w-5 text-red-500" />
                                     </button>
                                 </th>
                             </tr>)
